d28e1f6756c46fe15279a00bfa821f042221904d
Radhika <radhika@witbybit.com>
2018-11-16 13:19:37 +0530

    First commit

---
+++ app/src/main/java/com/assignment_radhika/instavans_assignment/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.assignment_radhika.instavans_assignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+    }
+}
