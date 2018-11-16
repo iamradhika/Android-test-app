e2ff5f5bad9ba3284e3c8b15268981d59cfa5f1e
Radhika <radhika@witbybit.com>
2018-11-16 14:04:29 +0530

    commit

--- app/src/main/java/com/assignment_radhika/instavans_assignment/SplashScreen.java
+++ app/src/main/java/com/assignment_radhika/instavans_assignment/SplashScreen.java
@@ -1,13 +1,36 @@
 package com.assignment_radhika.instavans_assignment;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
+
+
+        TextView bart =  findViewById(R.id.tv);
+        bart.animate().alpha(0f).setDuration(5000);
+
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MapsActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
     }
+
 }
